6d6e3e3f0e5a40da1939834c12d6f51a73cee417
juanmanoel <juanengml@gmail.com>
2020-11-06 00:52:34 +0000

    coloquei a poha do buscador

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from random import randint
 import random
 
 from EstruturaDados import Pilha, Filas,ListaEncadeadaSimples
-
 from Arvore import NodoArvore, insere, busca, Organiza
+
 
 keys = [
 {1: ["bola", "arq1.txt"]},
@@ -14,35 +14,50 @@ keys = [
 {3: ["dado", "arq2.txt"]},
 {1: ["bola", "arq2.txt"]},
 {4: ["arvore", "arq2.txt"]},
-{4: ["arvore", "arq1.txt"]}]
+{4: ["arvore", "arq1.txt"]},
+{4: ["arvore", "arq3.txt"]}]
 
-### ATP Estrutura Parte 2 
 
-def main():
-  
-  # inserindo valores na arvore
-  raiz = NodoArvore(0)
-  for chave in keys:
-    key = min(chave.keys())
-    print(key,chave[key])
-    nodo = NodoArvore(chave=key,lista=chave[key])
-    insere(raiz, nodo)
-    
+# ATP 3 
 
+def BuscadorATP3(raiz, termo):
+  #for chave in keys:
   # Procura por valores na árvore.
   ordenar = []
   for chave in keys:
     key = min(chave.keys())
     resultado = busca(raiz, key)
     if resultado:
-        print("Busca pela chave {}  Sucesso!".format(chave))
+        #print("Busca pela chave {}  Sucesso!".format(chave))
         ordenar.append(chave)
         
     else:
-        print("Busca pela chave {} : Falha!".format(chave))  
+        #print("Busca pela chave {} : Falha!".format(chave))  
+        pass 
   
   for arvore in Organiza(ordenar):
-    print(arvore)
+    #print(arvore)
+    key, value = list(arvore.items())[0]
+    if key == termo:
+        print("-"*30)
+        print('Termo = ',key,value[0] , "->" ,value[1])
+        print("-"*30)
+
+
+def main():
+  
+  # inserindo valores na arvore ATP 2 
+  raiz = NodoArvore(0)
+  for chave in keys:
+    key = min(chave.keys())
+    #print(key,chave[key])
+    nodo = NodoArvore(chave=key,lista=chave[key])
+    insere(raiz, nodo)
+    
+
+  # Procura por valores na árvore.
+  BuscadorATP3(raiz, "casa")
+  
         
 
 
